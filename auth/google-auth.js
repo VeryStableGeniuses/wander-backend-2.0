b805667a9dc903875bdefa2bf15f320c8598d1bd
Jessie White <jleveewhite@gmail.com>
2018-01-13 14:17:43 -0600

    [google-Auth] change strategy

--- auth/google-auth.js
+++ auth/google-auth.js
@@ -1,17 +1,1 @@
 require('dotenv').config();
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const passport = require('passport-google-oauth20');
-
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3030',
-  passReqToCallback   : true
-},
-
-function(accessToken, refreshToken, profile, cb) {
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    return cb(err, user);
-  });
-}
-));
