b9f51abde2100371794e515650a350f59f629414
Jessie White <jleveewhite@gmail.com>
2018-01-23 23:12:38 -0600

    [superchore] fix linter fanaticism

--- scheduleBuilder.js
+++ scheduleBuilder.js
@@ -7,9 +7,8 @@ const calculateDistance = (lat1, lon1, lat2, lon2) => {
   const radlat2 = Math.PI * lat2 / 180;
   const theta = lon1 - lon2;
   const radtheta = Math.PI * theta / 180;
-  let dist =
-    Math.sin(radlat1) * Math.sin(radlat2) +
-    Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1)
+    * Math.cos(radlat2) * Math.cos(radtheta);
   dist = Math.acos(dist);
   dist = dist * 180 / Math.PI;
   dist = dist * 60 * 1.1515;
@@ -21,13 +20,9 @@ const calculateDistance = (lat1, lon1, lat2, lon2) => {
 // how close that event is to the most recent event
 const makeRankings = (googleData, predictHQ, interests, dislikes) => {
   const possibilities = googleData
-    .filter(event => {
+    .filter((event) => {
       for (let i = 0; i < event.types.length; i++) {
-        if (
-          interests.includes(event.types[i]) &&
-          event.name !== 'Walking Tours' &&
-          !dislikes.includes(event.types[i])
-        ) {
+        if (interests.includes(event.types[i]) && event.name !== 'Walking Tours' && !dislikes.includes(event.types[i])) {
           return event;
         }
       }
@@ -35,7 +30,7 @@ const makeRankings = (googleData, predictHQ, interests, dislikes) => {
     .sort((a, b) => b.rating - a.rating);
 
   let sortedByDistance = possibilities.slice(1);
-  sortedByDistance.forEach(event => {
+  sortedByDistance.forEach((event) => {
     event.distanceFromTopRated = calculateDistance(
       possibilities[0].geometry.location.lat,
       possibilities[0].geometry.location.lng,
@@ -43,11 +38,10 @@ const makeRankings = (googleData, predictHQ, interests, dislikes) => {
       event.geometry.location.lng
     );
   });
-  sortedByDistance = sortedByDistance.sort(
-    (a, b) => a.distanceFromTopRated - b.distanceFromTopRated
-  );
+  sortedByDistance = sortedByDistance.sort((a, b) =>
+    a.distanceFromTopRated - b.distanceFromTopRated);
 
-  sortedByDistance.forEach(event => {
+  sortedByDistance.forEach((event) => {
     event.ranking = (event.rating - event.distanceFromTopRated) / 5;
   });
 
@@ -56,9 +50,9 @@ const makeRankings = (googleData, predictHQ, interests, dislikes) => {
   return sortedByDistance;
 };
 
-const sortByDistance = googleData => {
+const sortByDistance = (googleData) => {
   let sortedByDistance = googleData.slice(1);
-  sortedByDistance.forEach(event => {
+  sortedByDistance.forEach((event) => {
     event.distanceFromTopRated = calculateDistance(
       googleData[0].latlng.lat,
       googleData[0].latlng.lng,
@@ -66,9 +60,8 @@ const sortByDistance = googleData => {
       event.latlng.lng
     );
   });
-  sortedByDistance = sortedByDistance.sort(
-    (a, b) => a.distanceFromTopRated - b.distanceFromTopRated
-  );
+  sortedByDistance = sortedByDistance.sort((a, b) =>
+    a.distanceFromTopRated - b.distanceFromTopRated);
 
   sortedByDistance.unshift(googleData[0]);
   return sortedByDistance;
@@ -81,17 +74,14 @@ const findRestaurant = (location, placed, restaurants) => {
       location.latlng.lng,
       Number(restaurant.restaurant.location.latitude),
       Number(restaurant.restaurant.location.longitude)
-    )
-  );
-  const sorted = restaurants.sort(
-    (a, b) => a.distanceFromTopRated - b.distanceFromTopRated
-  );
+    ));
+  const sorted = restaurants.sort((a, b) => a.distanceFromTopRated - b.distanceFromTopRated);
   const result = {
     name: sorted[0].restaurant.name,
     location: {
       latitude: Number(sorted[0].restaurant.location.latitude),
-      longitude: Number(sorted[0].restaurant.location.longitude)
-    }
+      longitude: Number(sorted[0].restaurant.location.longitude), 
+    },
   };
   sorted.splice(0, 1);
   return result;
@@ -103,14 +93,11 @@ const fillDay = (day, rankedList, interests, restaurants) => {
   let currentEvent = 0;
   let eventsPlaced = 0;
   let restaurantsPlaced = 0;
-  while (timeSpent <= 12 && currentEvent < rankedList.length) {
+  while(timeSpent <= 12 && currentEvent < rankedList.length) {
     day[`event${++eventsPlaced}`] = {
       name: rankedList[currentEvent].name,
-      location: {
-        latitude: rankedList[currentEvent].geometry.location.lat,
-        longitude: rankedList[currentEvent].geometry.location.lng
-      },
-      googleId: rankedList[currentEvent].place_id
+      location: { latitude: rankedList[currentEvent].geometry.location.lat, longitude: rankedList[currentEvent].geometry.location.lng},
+      googleId: rankedList[currentEvent].place_id,
     };
     if (attractionTimes[rankedList[currentEvent].types[0]]) {
       timeSpent += attractionTimes[rankedList[currentEvent].types[0]];
@@ -121,62 +108,42 @@ const fillDay = (day, rankedList, interests, restaurants) => {
     currentEvent++;
   }
   while (restaurantsPlaced < 5) {
-    day[`restaurant${++restaurantsPlaced}`] = findRestaurant(
-      rankedList[0],
-      restaurantsPlaced,
-      restaurants
-    );
+    day[`restaurant${++restaurantsPlaced}`] = findRestaurant(rankedList[0], restaurantsPlaced, restaurants);
   }
 };
 
 // Here's where the magic happens
-const scheduleBuilder = (
-  startDate,
-  endDate,
-  google,
-  restaurantData,
-  predictHQ,
-  interests
-) => {
-  const filteredData = google.map(location => {
-    return {
-      name: location.name,
-      placeId: location.place_id,
-      rating: location.rating,
-      latlng: location.geometry.location
-    };
+const scheduleBuilder = (startDate, endDate, google, restaurantData, predictHQ, interests) => {
+  const filteredData = google.map((location) => {
+    return { name: location.name, placeId: location.place_id, rating: location.rating, latlng: location.geometry.location };
   });
 
   const sorted = sortByDistance(filteredData);
 
   startDate = new Date(startDate);
   endDate = new Date(endDate);
-
+  
   // Get the start day out
   let currentDate = startDate;
-
+  
   // Initialize the empty schedule object
   const schedule = {};
-
+  
   // Get the total number of days that the user will be in the destination
-  const numberOfDays = Math.round(
-    (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
-  );
-
+  const numberOfDays =
+    Math.round((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
+  
   // Fill the schedule with "day" objects
   for (let i = 1; i < numberOfDays + 2; i += 1) {
     schedule[`day_${i}`] = {};
   }
 
   const googleLength = google.length;
-
+  
   // Go through each day, fill it out with events
   const days = Object.keys(schedule);
-  days.forEach(day => {
-    schedule[day].events = sorted.splice(
-      0,
-      Math.floor(googleLength / (numberOfDays + 1))
-    );
+  days.forEach((day) => {
+    schedule[day].events = sorted.splice(0, Math.floor(googleLength / (numberOfDays + 1)));
     schedule[day].liveEvents = [];
     schedule[day].restaurants = [];
     predictHQ.forEach((event, i) => {
@@ -186,12 +153,11 @@ const scheduleBuilder = (
       }
     });
     for (let i = 0; i < 4; i++) {
-      schedule[day].restaurants.push(
-        findRestaurant(schedule[day].events[0], 0, restaurantData)
-      );
+      schedule[day].restaurants.push(findRestaurant(schedule[day].events[0], 0, restaurantData));
     }
     schedule[day].date = new Date(currentDate);
     currentDate.setDate(currentDate.getDate() + 1);
+
   });
   return schedule;
 };
@@ -200,71 +166,37 @@ const getSchedule = (startDate, endDate, location, interests, cb) => {
   const query = location.split(' ').join('+');
   const zomatoConfig = {
     headers: {
-      'user-key': keys.zomato
-    }
+      'user-key': keys.zomato,
+    },
   };
   const predictHQconfig = {
     headers: {
-      Authorization: `Bearer ${keys.predictHQToken}`
-    }
+      Authorization: `Bearer ${keys.predictHQToken}`,
+    },
   };
   let googleData = [];
-  axios
-    .get(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}+point+of+interest&language=en&key=${
-        keys.googlePlacesAPI
-      }`
-    )
-    .then(response1 => {
+  axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}+point+of+interest&language=en&key=${keys.googlePlacesAPI}`)
+    .then((response1) => {
       googleData = googleData.concat(response1.data.results);
-      return axios.get(
-        `https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken=${
-          response1.data.next_page_token
-        }&key=${keys.googlePlacesAPI}`
-      );
+      return axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken=${response1.data.next_page_token}&key=${keys.googlePlacesAPI}`)
     })
-    .then(response2 => {
+    .then((response2) => {
       if (response2.data.results.length) {
         googleData = googleData.concat(response2.data.results);
       }
-      return axios.get(
-        `https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken=${
-          response2.data.next_page_token
-        }&key=${keys.googlePlacesAPI}`
-      );
+      return axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken=${response2.data.next_page_token}&key=${keys.googlePlacesAPI}`)
     })
-    .then(response3 => {
+    .then((response3) => {
       if (response3.data.results.length) {
         googleData = googleData.concat(response3.data.results);
       }
       return Promise.all([
-        axios.get(
-          `https://api.predicthq.com/v1/events/?category=concerts,festivals,performing-arts,sports&within=10mi@${
-            googleData[0].geometry.location.lat
-          },${
-            googleData[0].geometry.location.lng
-          }.0060&start.gte=2018-02-11&start.lte=2018-02-17&rank_level=4,5`,
-          predictHQconfig
-        ),
-        axios.get(
-          `https://developers.zomato.com/api/v2.1/search?lat=${
-            googleData[0].geometry.location.lat
-          }&lon=${googleData[0].geometry.location.lng}&sort=rating`,
-          zomatoConfig
-        )
+        axios.get(`https://api.predicthq.com/v1/events/?category=concerts,festivals,performing-arts,sports&within=10mi@${googleData[0].geometry.location.lat},${googleData[0].geometry.location.lng}.0060&start.gte=2018-02-11&start.lte=2018-02-17&rank_level=4,5`, predictHQconfig),
+        axios.get(`https://developers.zomato.com/api/v2.1/search?lat=${googleData[0].geometry.location.lat}&lon=${googleData[0].geometry.location.lng}&sort=rating`, zomatoConfig),
       ]);
     })
     .then(([predictHQ, zomato]) => {
-      cb(
-        scheduleBuilder(
-          startDate,
-          endDate,
-          googleData,
-          zomato.data.restaurants,
-          predictHQ.data.results,
-          interests
-        )
-      );
+      cb(scheduleBuilder(startDate, endDate, googleData, zomato.data.restaurants, predictHQ.data.results, interests));
     })
     .catch(err => console.error(err));
 };
