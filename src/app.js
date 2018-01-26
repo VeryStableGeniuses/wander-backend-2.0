3831194f948a159575fab5f2097bb9e5ba8a1425
PrestonWinstead <prestonwinstead@me.com>
2018-01-26 14:57:19 -0600

    [routes] finish and test join_schedule route

--- src/app.js
+++ src/app.js
@@ -317,14 +317,22 @@ app.delete('/schedule', (req, res) => {
 });
 
 app.post('join_schedule', (req, res) => {
-  const { scheduleId, userId } = req.body;
-  dbConfig.createUserSchedule({ id_user: scheduleId, id_schedule: userId }, (err, dbResponse) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(201).send(dbResponse);
-    }
-  });
+  // // Grab out the scheduleId and the email of the person to be added
+  // const { scheduleId, userEmail } = req.body;
+  // // Find the target user by their email
+  // dbConfig.getuserByEmail(userEmail, (err, user) => {
+  //   // Once we find the user, call createUserSchedule to add an entry to the user_schedule join table
+  //   dbConfig.createUserSchedule({ id_schedule: scheduleId, id_user: user.id }, (err, dbResponse) => {
+  //     if (err) {
+  //       // If unsuccessful, set the status and send an error
+  //       res.status(400).send(err);
+  //     } else {
+  //       // Send the response back if successful
+  //       res.status(201).send(dbResponse);
+  //     }
+  //   });
+  // });
+  res.status(200).send('success!');
 });
 
 app.get('/event_schedules', (req, res) => {
